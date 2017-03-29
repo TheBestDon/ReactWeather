918b84e57864b211055df7ab94ea26369d57ac81
TheBestDon <donatas_bakanas@yahoo.com>
2017-03-29 20:56:45 +0300

    About styling

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,8 +5,20 @@ var React = require('react');
 var About = (props) => {
     return(
     <div>
-        <h3>About</h3>
-        <p>Welcome to About page!</p> 
+       <h1 className="text-center">About</h1>
+        <p>This is a weather application build on React. Built on learning purpuses.</p>
+        <p>
+            Here are some of the tools I used:
+        </p>
+        <ul>
+            <li>
+                <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used.
+            </li>
+            <li>
+                <a href="http://openweathermap.org">Open Weather Map</a> - I used Open Weather Map API to search for weather data by city name.
+            </li>
+        </ul>
+    </div>
     </div>
        
         )
